9d6d45659ed44e893dce00882e6071edbde66c91
Matheus Amazonas <matheus.amazonas@gmail.com>
2026-07-06 17:35:47 +0200

    ✨ Add utility parsers for word, words with spaces and characters that are not empty spaces
    They should be used for many future parsers. Tests was also added.

--- src/parsing/utils.rs
+++ src/parsing/utils.rs
@@ -1,4 +1,4 @@
-use yapcol::{Parser, StringParser, choice, either, is};
+use yapcol::{Parser, StringParser, choice, either, is, satisfy};
 
 pub(crate) trait EmptyParser: StringParser<()> {}
 
@@ -25,7 +25,30 @@ pub(crate) fn parse_line_end() -> impl EmptyParser {
 }
 
 pub(crate) fn parse_empty_line() -> impl EmptyParser {
-	parse_spaces_or_tabs().and(parse_line_end())
+	parse_spaces_or_tabs().maybe().and(parse_line_end())
+}
+
+pub(crate) fn parse_not_empty_space() -> impl StringParser<char> {
+	satisfy(|&c| match c {
+		' ' => None,
+		'\t' => None,
+		'\n' => None,
+		c => Some(c),
+	})
+}
+
+pub(crate) fn parse_word() -> impl StringParser<String> {
+	parse_not_empty_space()
+		.once_or_more_collect()
+		.map(|chars| chars.into_iter().collect())
+}
+
+pub(crate) fn parse_word_with_spaces() -> impl StringParser<String> {
+	|input| {
+		let word = parse_word()(input)?;
+		parse_spaces_or_tabs().maybe()(input)?;
+		Ok(word)
+	}
 }
 
 #[cfg(test)]
@@ -34,42 +57,48 @@ mod tests {
 	#[cfg(test)]
 	mod spaces_or_tabs {
 		use crate::parsing::utils::parse_spaces_or_tabs;
-		use yapcol::Input;
+		use yapcol::{Input, end_of_input};
 
 		#[test]
 		fn empty_succeeds() {
 			let mut input = Input::new_from_chars("".chars(), None);
 			parse_spaces_or_tabs()(&mut input).unwrap();
+			end_of_input()(&mut input).unwrap();
 		}
 
 		#[test]
 		fn only_spaces_succeeds() {
 			let mut input = Input::new_from_chars("     ".chars(), None);
 			parse_spaces_or_tabs()(&mut input).unwrap();
+			end_of_input()(&mut input).unwrap();
 		}
 
 		#[test]
 		fn only_tabs_succeeds() {
 			let mut input = Input::new_from_chars("\t\t\t".chars(), None);
 			parse_spaces_or_tabs()(&mut input).unwrap();
+			end_of_input()(&mut input).unwrap();
 		}
 
 		#[test]
 		fn tab_after_space_succeeds() {
 			let mut input = Input::new_from_chars(" \t".chars(), None);
 			parse_spaces_or_tabs()(&mut input).unwrap();
+			end_of_input()(&mut input).unwrap();
 		}
 
 		#[test]
 		fn space_after_tab_succeeds() {
 			let mut input = Input::new_from_chars("\t ".chars(), None);
 			parse_spaces_or_tabs()(&mut input).unwrap();
+			end_of_input()(&mut input).unwrap();
 		}
 
 		#[test]
 		fn mixed_succeeds() {
 			let mut input = Input::new_from_chars(" \t\t  \t \t  ".chars(), None);
 			parse_spaces_or_tabs()(&mut input).unwrap();
+			end_of_input()(&mut input).unwrap();
 		}
 
 		#[test]
@@ -232,6 +261,124 @@ mod tests {
 			parse_empty_line()(&mut input).unwrap();
 			let mut input = Input::new_from_chars("   \t\r\n".chars(), None);
 			parse_empty_line()(&mut input).unwrap();
+		}
+	}
+
+	#[cfg(test)]
+	mod not_empty_space {
+		use crate::parsing::utils::parse_not_empty_space;
+		use yapcol::Input;
+
+		#[test]
+		fn whitespace_fails() {
+			let mut input = Input::new_from_chars(" ".chars(), None);
+			assert!(parse_not_empty_space()(&mut input).is_err());
+		}
+
+		#[test]
+		fn tab_fails() {
+			let mut input = Input::new_from_chars("\t".chars(), None);
+			assert!(parse_not_empty_space()(&mut input).is_err());
+		}
+
+		#[test]
+		fn line_feed_fails() {
+			let mut input = Input::new_from_chars("\n".chars(), None);
+			assert!(parse_not_empty_space()(&mut input).is_err());
+		}
+
+		#[test]
+		fn others_fail() {
+			let mut input = Input::new_from_chars("a".chars(), None);
+			parse_not_empty_space()(&mut input).unwrap();
+			let mut input = Input::new_from_chars("1".chars(), None);
+			parse_not_empty_space()(&mut input).unwrap();
+			let mut input = Input::new_from_chars("@".chars(), None);
+			parse_not_empty_space()(&mut input).unwrap();
+		}
+	}
+
+	#[cfg(test)]
+	mod word {
+		use crate::parsing::utils::parse_word;
+		use yapcol::Input;
+
+		#[test]
+		fn one_word_no_space_succeeds() {
+			let mut input = Input::new_from_chars("test".chars(), None);
+			let output = parse_word()(&mut input).unwrap();
+			assert_eq!(output, "test".to_string());
+		}
+
+		#[test]
+		fn whitespace_fails() {
+			let mut input = Input::new_from_chars(" ".chars(), None);
+			assert!(parse_word()(&mut input).is_err());
+		}
+
+		#[test]
+		fn tab_fails() {
+			let mut input = Input::new_from_chars("\t".chars(), None);
+			assert!(parse_word()(&mut input).is_err());
+		}
+
+		#[test]
+		fn one_word_with_space_fails() {
+			let mut input = Input::new_from_chars(" test".chars(), None);
+			assert!(parse_word()(&mut input).is_err());
+		}
+
+		#[test]
+		fn one_word_with_tab_fails() {
+			let mut input = Input::new_from_chars("\ttest".chars(), None);
+			assert!(parse_word()(&mut input).is_err());
+		}
+
+		#[test]
+		fn line_break_fails() {
+			let mut input = Input::new_from_chars("\n".chars(), None);
+			assert!(parse_word()(&mut input).is_err());
+		}
+	}
+
+	#[cfg(test)]
+	mod word_with_spaces {
+		use crate::parsing::utils::parse_word_with_spaces;
+		use yapcol::Input;
+
+		#[test]
+		fn no_spaces_succeeds() {
+			let mut input = Input::new_from_chars("test".chars(), None);
+			let output = parse_word_with_spaces()(&mut input).unwrap();
+			assert_eq!(output, String::from("test"));
+		}
+
+		#[test]
+		fn one_space_succeeds() {
+			let mut input = Input::new_from_chars("test ".chars(), None);
+			let output = parse_word_with_spaces()(&mut input).unwrap();
+			assert_eq!(output, String::from("test"));
+		}
+
+		#[test]
+		fn one_tab_succeeds() {
+			let mut input = Input::new_from_chars("test\t".chars(), None);
+			let output = parse_word_with_spaces()(&mut input).unwrap();
+			assert_eq!(output, String::from("test"));
+		}
+
+		#[test]
+		fn many_spaces_succeeds() {
+			let mut input = Input::new_from_chars("test    ".chars(), None);
+			let output = parse_word_with_spaces()(&mut input).unwrap();
+			assert_eq!(output, String::from("test"));
+		}
+
+		#[test]
+		fn many_tabs_succeeds() {
+			let mut input = Input::new_from_chars("test\t\t\t\t\t".chars(), None);
+			let output = parse_word_with_spaces()(&mut input).unwrap();
+			assert_eq!(output, String::from("test"));
 		}
 	}
 }
